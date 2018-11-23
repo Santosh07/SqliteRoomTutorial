c54d51b01fe378dfdb1e948738438350fd9757f8
Santosh Dhakal <santosh.dhakal07@gmail.com>
2018-11-23 16:37:45 +0545

    fixed dagger bugs

--- app/src/main/java/com/santosh/sqlitetutorial/di/components/AppComponent.kt
+++ app/src/main/java/com/santosh/sqlitetutorial/di/components/AppComponent.kt
@@ -2,12 +2,13 @@ package com.santosh.sqlitetutorial.di
 
 import android.app.Application
 import com.santosh.sqlitetutorial.di.modules.AppModule
+import com.santosh.sqlitetutorial.di.modules.ViewModelFactoryModule
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AppModule::class])
+@Component(modules = [AppModule::class, ViewModelFactoryModule::class])
 interface AppComponent {
 
     @Component.Builder
